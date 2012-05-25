c9a8b59adbe9c17f814119d1a09910e0af603dd5
Jason Bartels <jbartels@rackspace.com>
2012-05-25 13:55:51 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+#  Routing for the stats application
+
+@app.route('/')
+def home():
+	"""Render website's home page."""
+	return render_template('base.html')
+
+
+#  Functions below can be used for all Flask apps
+
+@app.errorhandler(404)
+def page_not_found(error):
+	"""Custom 404 page."""
+	return render_template('404.html')
+
+	
+if __name__ == '__main__':
+	app.run(debug=True)
